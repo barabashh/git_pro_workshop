1908dcd356f9b42e792acf8f0fdf6fa33396215f
Áron Esztergomi <aaron.esztergomi@gmail.com>
2020-05-25 15:33:48 +0200

    d4

--- src/Greet.java
+++ src/Greet.java
@@ -1,10 +1,6 @@
 public class Greet {
-<<<<<<< HEAD
+
   public void greet(){
     System.out.println("Greetings!");
-=======
-  public void greet(String name){
-    System.out.println(name);
->>>>>>> bf01965d7d686d2a55deb2db602fbf7a5264a329
   }
 }
